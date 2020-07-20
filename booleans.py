2f689360a3dec15fbd8e515078879b0ca0d1bfdf
DJ Viking <anders.swedishviking@gmail.com>
2020-07-20 10:38:49 -0400

    finished booleans.py with an if true statement

---
+++ booleans.py
@@ -0,0 +1,13 @@
+import sys
+
+python_is_glorious = True
+failure_is_option = False
+proper_greeting = False
+
+a_string = 'For the glory of Python!'
+
+
+if sys.argv[1] == a_string:
+    proper_greeting = True
+    print(proper_greeting)
+
